95553c1259598f178ec2fbd3bed138047d905cc4
Fanaen <contact@fanaen.fr>
2015-04-21 00:02:02 +0200

    Add Basic text

--- love2d/game.lua
+++ love2d/game.lua
@@ -13,7 +13,8 @@ function Game:load()
   self.textBlinkState   = true  -- Cursor's current state
   self.textCursorLine   = 1     -- Cursor's line
   self.textCursorMax    = 10    -- Nb line max
-  self.textCursorText   = ""    -- Current text
+  self.textCursorText   = "Test"    -- Current text
+  self.textFont         = love.graphics.newFont("fonts/UbuntuMono-R.ttf", 20)
   
   -- Background --
   self.background       = love.graphics.newImage("images/background.png")
@@ -44,10 +45,22 @@ function Game:draw()
   -- Background --
   love.graphics.draw(self.background, 0, 0)
   
+  -- Line --
+  local lineY = MARGIN_TOP + self.textCursorLine * LINES_HEIGHT
+  
+  -- Text --
+  if self.textCursorText ~= "" then
+    local x = 10
+    local y = lineY
+  
+    love.graphics.setFont(self.textFont)
+    love.graphics.print(self.textCursorText, x, y)
+  end
+  
   -- Cursor --
   if self.textBlinkState then
     local x = 10
-    local y = MARGIN_TOP + self.textCursorLine * LINES_HEIGHT
+    local y = lineY
     love.graphics.rectangle("fill", x, y, 4, 20)
   end
   
@@ -58,8 +71,22 @@ end
 function Game:onkeypressed(k)
 
   -- Fanaen Code --
+  if key == "backspace" then
+    self:backspace()
+  elseif key == "return" then
+    self:enter()
+  else
+    self.textCursorText = self.textCursorText .. key
+  end
   
-
   -- Psyko Code --
 	
+end
+
+function Game:enter()
+  
+end
+
+function Game:backspace()
+
 end
